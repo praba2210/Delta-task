d42a01d4bd995dc733b37fd88e9090dc3017c6b9
Prabakaran A <alchemist@fossfreaks.com>
2022-02-23 21:04:08 +0530

    Finished the login part

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector } from "react-redux";
+import { selectUSer } from "./features/userSlice";
+import React from 'react';
+import Logout from "./Components/Logout";
+import Login from './Components/Login';
+import "./App.css"
 
-function App() {
+export const App = () => {
+    const user =useSelector(selectUSer);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       {user ? <Logout /> : <Login />}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Components/Logout.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react'
+import "./Logout.css";
+import { useDispatch } from 'react-redux';
+import { logout } from '../features/userSlice';
+
+
+const Logout = () => {
+    const dispatch = useDispatch(); 
+    const handleLogout = (e) => {
+        e.preventDefault();
+        
+        dispatch(logout());
+    }
+  return (
+    <div className="logout">
+       <h1> Welcome <span className="user__name" >Prabakaran</span></h1>
+        <button className="logout__btn" onClick={(e) => handleLogout(e)}>
+            Logout
+            </button>
+    </div>
+  )
+}
+
+export default Logout
